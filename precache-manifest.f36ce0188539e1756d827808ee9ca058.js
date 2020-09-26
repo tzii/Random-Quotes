c1dbd896ceb42a0430593720c806d00c3d1b1979
Tuấn Vũ <tuanvundvn@gmail.com>
2020-09-26 20:12:50 +0700

    Updates

---
+++ precache-manifest.f36ce0188539e1756d827808ee9ca058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f634b580d4dcd9976290b6e20c0a5952",
+    "url": "/Random-Quotes/index.html"
+  },
+  {
+    "revision": "6b9070646fa53b557884",
+    "url": "/Random-Quotes/static/css/main.af5dbc2f.chunk.css"
+  },
+  {
+    "revision": "c59c58ac3f2903a88757",
+    "url": "/Random-Quotes/static/js/2.6be5e2e0.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/Random-Quotes/static/js/2.6be5e2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9070646fa53b557884",
+    "url": "/Random-Quotes/static/js/main.0e31b68c.chunk.js"
+  },
+  {
+    "revision": "afbcca5b78fca64c931d",
+    "url": "/Random-Quotes/static/js/runtime-main.49f7b301.js"
+  }
+]);
